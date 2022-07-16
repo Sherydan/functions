171aaa06b7e05f291f4823b8533baea50a10c3fe
Sherydan <tobar.luis.c@gmail.com>
2022-07-15 21:48:54 -0400

    added comments to my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
+/*
+
+  Modify this code to:
+  1. recveive the clicked element as an argument
+  2. pass a color as an argument
+
+
+  function pintar(){
+  ele = document.getElementById("ele1")
+  ele.style.backgroundColor = 'yellow'
+  }
+  ele = document.getElementById("ele1")
+  ele.addEventListener("click", pintar);
+
+  @ele: element you want to paint
+*/
+
+//select the element you want to paint
 let ele = document.getElementById("ele1");
 
-function pintar(element, color = "green") {
+// set color depending on waht arguments are passed
+function paint(element, color = "green") {
   element.style.backgroundColor = color;
 }
 
-ele.addEventListener("click", () => {
-    
-});
+// add listener to the element and call paint function
+ele.addEventListener("click", function(){paint(ele, "yellow");});
 
 
